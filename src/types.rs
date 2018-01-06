a447f2c1af7cffac01d5beeca161616efce98331
Sieu Tsoi <sieut@umich.edu>
2018-01-06 15:49:10 -0500

    Add Int type

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,29 @@
-pub trait Castable {
-    type Type;
+pub trait Type {
+    type SType;
+    type CType;
 
-    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::Type>;
+    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::CType>;
+    fn get_value(&self) -> Option<Self::CType>;
+    fn get_size(&self) -> usize;
+}
+
+pub enum ColumnType {
+    Int(Integer),
+}
+
+struct Integer(i32);
+
+impl Type for Integer {
+    type SType = Integer;
+    type CType = i32;
+
+    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::CType> {
+        Some(0)
+    }
+
+    fn get_value(&self) -> Option<Self::CType> {
+        Some(self.0)
+    }
+
+    fn get_size(&self) -> usize { 4 }
 }
