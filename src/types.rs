dbef21cdceb7186e2104198342ef87e64a1b6f4b
Sieu Tsoi <sieut@umich.edu>
2018-01-06 19:09:10 -0500

    Fix types.rs a bit

--- src/types.rs
+++ src/types.rs
@@ -1,10 +1,14 @@
+use std::cmp::Ordering;
+
 pub trait Type {
     type SType;
     type CType;
 
-    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::CType>;
-    fn get_value(&self) -> Option<Self::CType>;
+    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::SType>;
+    fn get_value(&self) -> Self::CType;
     fn get_size(&self) -> usize;
+
+    fn compare(&self, rhs: Self::SType) -> Ordering;
 }
 
 pub enum ColumnType {
@@ -17,13 +21,17 @@ impl Type for Integer {
     type SType = Integer;
     type CType = i32;
 
-    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::CType> {
-        Some(0)
+    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::SType> {
+        Some(Integer(0))
     }
 
-    fn get_value(&self) -> Option<Self::CType> {
-        Some(self.0)
+    fn get_value(&self) -> Self::CType {
+        self.0
     }
 
     fn get_size(&self) -> usize { 4 }
+
+    fn compare(&self, rhs: Self::SType) -> Ordering {
+        self.0.cmp(&rhs.get_value())
+    }
 }
