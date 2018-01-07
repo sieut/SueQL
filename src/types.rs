a2eeef5e4b833a122460f70b349aafe09bec59ec
Sieu Tsoi <sieut@umich.edu>
2018-01-06 20:54:11 -0500

    Change from_bytes of Type, implement from_bytes for Integer

--- src/types.rs
+++ src/types.rs
@@ -4,7 +4,7 @@ pub trait Type {
     type SType;
     type CType;
 
-    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::SType>;
+    fn from_bytes(bytes: &[u8]) -> Option<Self::SType>;
     fn get_value(&self) -> Self::CType;
     fn get_size(&self) -> usize;
 
@@ -15,14 +15,24 @@ pub enum ColumnType {
     Int(Integer),
 }
 
-struct Integer(i32);
+pub struct Integer(i32);
 
 impl Type for Integer {
     type SType = Integer;
     type CType = i32;
 
-    fn from_bytes(&self, bytes: &[u8]) -> Option<Self::SType> {
-        Some(Integer(0))
+    fn from_bytes(bytes: &[u8]) -> Option<Self::SType> {
+        if bytes.len() != 4 {
+            None
+        }
+        else {
+            let mut int_value:i32 = 0;
+            int_value |= ((bytes[0] as i32) << 24);
+            int_value |= ((bytes[1] as i32) << 16);
+            int_value |= ((bytes[2] as i32) << 8);
+            int_value |= (bytes[3] as i32);
+            Some(Integer(int_value))
+        }
     }
 
     fn get_value(&self) -> Self::CType {
