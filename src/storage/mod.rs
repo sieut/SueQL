bcd7e708e6458704944ffa01a210263e13438912
Sieu Tsoi <sieut@umich.edu>
2018-01-27 08:24:13 -0500

    Change 'use' and 'mod' again in storage's mod.rs

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,7 +1,9 @@
 pub const PAGE_SIZE:usize = 4096;
 
 pub use self::bufpage::BufPage;
+pub use self::pagereader::PageReader;
+pub use self::pagewriter::PageWriter;
 
 mod bufpage;
-pub mod pagereader;
-pub mod pagewriter;
+mod pagereader;
+mod pagewriter;
