cce88ce6935f60874c856813dcc02da91cdaa09a
Sieu Tsoi <sieut@umich.edu>
2018-02-07 16:11:17 -0500

    Add row_count for Table

--- src/table.rs
+++ src/table.rs
@@ -1,14 +1,20 @@
+extern crate byteorder;
+
 use storage::{Storable, PAGE_SIZE};
 use types;
 use utils;
+use self::byteorder::{ReadBytesExt, WriteBytesExt, LittleEndian};
+use std::io::Cursor;
 
 /// Table's name is max 31 bytes long, aligning with Column's size, for now
 pub struct Table {
+    row_count: u64,
     name: String,
     columns: Vec<Column>
 }
 
 /// Storage format for Table:
+///     - row_count: 64 bytes
 ///     - name: 32 bytes (max 31 bytes + NULL ending bytes)
 ///     - columns: rest of page
 /// Total: 4096 bytes (a page)
@@ -16,24 +22,28 @@ impl Storable for Table {
     fn from_bytes(bytes: &[u8]) -> Option<Self> {
         if bytes.len() != Self::get_size().unwrap() { return None; }
 
-        let name = utils::string_from_bytes(&bytes[0..32]).unwrap();
+        let mut rc_rdr = Cursor::new(&bytes[0..64]);
+        let row_count = rc_rdr.read_u64::<LittleEndian>().unwrap();
+
+        let name = utils::string_from_bytes(&bytes[64..96]).unwrap();
 
         let mut columns = vec![];
         let mut iter = bytes.chunks(32);
-        iter.next();                    // Skip the first chunk (table_name)
+        iter.next(); iter.next(); iter.next();            // Skip the first 3 chunks (row_count, table_name)
         while let Some(chunk) = iter.next() {
             let col = Column::from_bytes(&chunk);
             if col.is_some() { columns.push(col.unwrap()); }
             else { break; }
         }
 
-        Some(Table { name: name, columns: columns })
+        Some(Table { row_count: row_count, name: name, columns: columns })
     }
 
     // TODO implement
     fn to_bytes(&self) -> Option<Vec<u8>> {
         let mut ret:Vec<u8> = vec![];
 
+        ret.write_u64::<LittleEndian>(self.row_count).unwrap();
         ret.append(&mut utils::string_to_bytes(&self.name, 32).unwrap());
 
         for col in self.columns.iter() {
