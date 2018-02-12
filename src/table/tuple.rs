1443195268447eef111bbede8e8142ec9e17a79e
Sieu Tsoi <sieut@umich.edu>
2018-02-12 05:56:50 -0500

    Add comments for TupleDesc struct

--- src/table/tuple.rs
+++ src/table/tuple.rs
@@ -1,5 +1,9 @@
 use table::column::Column;
 
+/// Tuple Descriptor struct
+///     - columns: vector of Columns which have column's name and data type
+///     - paddings: vector of padding bytes of each column to have the fields aligned
+///     - tuple_size: size, in bytes, of a row described by this TupleDesc, including padding bytes
 pub struct TupleDesc {
     pub columns: Vec<Column>,
     pub paddings: Vec<usize>,
