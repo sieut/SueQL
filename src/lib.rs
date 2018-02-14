c8b8c223a5a2fbea4937fb4d8bacb1688d35c760
Sieu Tsoi <sieut@umich.edu>
2018-02-14 06:55:23 -0500

    Implemented a crappy lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+#[macro_use] extern crate lazy_static;
+
 pub mod database;
 pub mod table;
 mod storage;
+mod sql;
 mod sort;
 mod types;
 mod utils;
---
+++ src/sql/mod.rs
@@ -0,0 +1,3 @@
+/// I'm only parsing SQL minimum grammar, according to MS, for now
+/// Link: (https://docs.microsoft.com/en-us/sql/odbc/reference/appendixes/sql-minimum-grammar)
+mod lexer;
