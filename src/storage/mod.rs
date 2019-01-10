4a3823814d384e5ef239cccf95ce09ede23adb34
Sieu Tsoi <sieut@umich.edu>
2019-01-10 02:25:12 -0500

    Clean up

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,10 +1,6 @@
 pub const PAGE_SIZE:usize = 4096;
 
-pub use self::pagereader::PageReader;
-pub use self::pagewriter::PageWriter;
 pub use self::storable::Storable;
 
 pub mod bufpage;
-mod pagereader;
-mod pagewriter;
 mod storable;
