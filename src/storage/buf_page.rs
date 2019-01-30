4f3b5921eb895c1de9647d20d0560cc811804221
Sieu Tsoi <sieut@umich.edu>
2019-01-30 16:56:25 -0500

    Added buf_page_tests and fix some bugs in buf_page

--- src/storage/buf_page.rs
+++ src/storage/buf_page.rs
@@ -10,7 +10,7 @@ use tuple::tuple_ptr::TuplePtr;
 
 static HEADER_SIZE: usize = 8;
 static UPPER_PTR_OFFSET: PagePtr = 0;
-static LOWER_PTR_OFFSET: PagePtr = 8;
+static LOWER_PTR_OFFSET: PagePtr = 4;
 
 // Page layout will be similar to Postgres' (http://www.interdb.jp/pg/pgsql01.html#_1.3.)
 pub struct BufPage {
@@ -43,6 +43,8 @@ impl BufPage {
                             tuple_data: &[u8],
                             tuple_ptr: Option<&TuplePtr>)
             -> Result<PagePtr, std::io::Error> {
+        let ret_offset;
+
         let buf_offset = match tuple_ptr {
             Some(ptr) => {
                 self.is_valid_tuple_ptr(ptr)?;
@@ -51,6 +53,8 @@ impl BufPage {
                 if self.tuple_data_len(ptr)? != tuple_data.len() {
                     panic!("Different sized tuple");
                 }
+
+                ret_offset = ptr.buf_offset();
 
                 let read_lock = self.buf.read().unwrap();
                 let mut reader = Cursor::new(
@@ -70,8 +74,9 @@ impl BufPage {
                     &mut write_lock[self.lower_ptr as usize
                         ..(self.lower_ptr + 4) as usize],
                     new_ptr);
+                ret_offset = self.lower_ptr;
 
-                self.lower_ptr += 8;
+                self.lower_ptr += 4;
                 LittleEndian::write_u32(
                     &mut write_lock[LOWER_PTR_OFFSET as usize
                         ..(LOWER_PTR_OFFSET + 4) as usize],
@@ -91,7 +96,7 @@ impl BufPage {
         write_lock[buf_offset as usize..buf_offset as usize + tuple_data.len()]
             .clone_from_slice(tuple_data);
 
-        Ok(buf_offset)
+        Ok(ret_offset)
     }
 
     pub fn get_tuple_data_range(&self, tuple_ptr: &TuplePtr)
@@ -99,19 +104,18 @@ impl BufPage {
         self.is_valid_tuple_ptr(tuple_ptr)?;
         let read_lock = self.buf.read().unwrap();
         let mut reader = Cursor::new(
-            &read_lock[(tuple_ptr.buf_offset() - 8) as usize
-            ..tuple_ptr.buf_offset() as usize]);
+            &read_lock[(tuple_ptr.buf_offset() - 4) as usize
+            ..(tuple_ptr.buf_offset() + 4) as usize]);
 
+        let prev_u32 = reader.read_u32::<LittleEndian>()?;
         let end =
             if tuple_ptr.buf_offset() as usize == HEADER_SIZE {
-                // Skip the current u32 value at the cursor
-                reader.read_u32::<LittleEndian>().unwrap();
                 PAGE_SIZE
             }
             else {
-                reader.read_u32::<LittleEndian>().unwrap()
+                prev_u32
             };
-        let start = reader.read_u32::<LittleEndian>().unwrap();
+        let start = reader.read_u32::<LittleEndian>()?;
 
         Ok(start as usize..end as usize)
     }
--- src/storage/tests/mod.rs
+++ src/storage/tests/mod.rs
@@ -1,1 +1,2 @@
 mod buf_mgr_tests;
+mod buf_page_tests;
