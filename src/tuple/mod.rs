41a4d5cec977f11d05fd94bdf88d0b0b37b6d7ac
Sieu Tsoi <sieut@umich.edu>
2019-01-30 23:22:09 -0500

    Create tuple data from input strings

--- src/tuple/mod.rs
+++ src/tuple/mod.rs
@@ -1,5 +1,7 @@
-pub use self::tuple::Tuple;
-
-mod tuple;
 pub mod tuple_desc;
 pub mod tuple_ptr;
+
+use self::tuple_desc::TupleDesc;
+use self::tuple_ptr::TuplePtr;
+
+pub type TupleData = Vec<u8>;
