b17f008dfb9bc3151f72037e52eaf7791aac9912
Sieu Tsoi <sieut@umich.edu>
2019-01-30 23:22:09 -0500

    Create tuple data from input strings

--- src/tuple/tuple_desc.rs
+++ src/tuple/tuple_desc.rs
@@ -1,4 +1,5 @@
 use data_type::DataType;
+use tuple::TupleData;
 
 pub struct TupleDesc {
     attrs_types: Vec<DataType>,
@@ -7,5 +8,16 @@ pub struct TupleDesc {
 impl TupleDesc {
     pub fn new(attrs_types: Vec<DataType>) -> TupleDesc {
         TupleDesc { attrs_types }
+    }
+
+    pub fn create_tuple_data(&self, inputs: Vec<String>) -> TupleData {
+        let mut data = vec![];
+        for (index, input) in inputs.iter().enumerate() {
+            let data_type = self.attrs_types.get(index).unwrap();
+            let mut bytes = data_type.string_to_bytes(&input).unwrap();
+            data.append(&mut bytes);
+        }
+
+        data
     }
 }
