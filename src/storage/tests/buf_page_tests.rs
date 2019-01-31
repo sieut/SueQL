e9ebf33a4fb6d8e901fe2fec3e0e03f5e670fb90
Sieu Tsoi <sieut@umich.edu>
2019-01-31 13:12:47 -0500

    Update buf_offset to 0-index and change types to usize

--- src/storage/tests/buf_page_tests.rs
+++ src/storage/tests/buf_page_tests.rs
@@ -10,8 +10,8 @@ use tuple::tuple_ptr::TuplePtr;
 
 #[test]
 fn test_write_new_tuple() {
-    let mut buffer: [u8; PAGE_SIZE as usize] = [0; PAGE_SIZE as usize];
-    LittleEndian::write_u32(&mut buffer[0..4], PAGE_SIZE);
+    let mut buffer: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
+    LittleEndian::write_u32(&mut buffer[0..4], PAGE_SIZE as u32);
     LittleEndian::write_u32(&mut buffer[4..8], 8);
 
     let mut buf_page = BufPage::load_from(
@@ -19,16 +19,19 @@ fn test_write_new_tuple() {
 
     let test_data = [5; 16];
     let buf_offset = buf_page.write_tuple_data(&test_data, None).unwrap();
-    assert_eq!(buf_offset, 8);
+    assert_eq!(buf_offset, 0);
 
     let read_lock = buf_page.buf.read().unwrap();
     let mut reader = Cursor::new(&read_lock[0..12]);
     // upper_ptr
-    assert_eq!(reader.read_u32::<LittleEndian>().unwrap(), PAGE_SIZE - 16);
+    assert_eq!(reader.read_u32::<LittleEndian>().unwrap() as usize,
+               PAGE_SIZE - 16);
     // lower_ptr
-    assert_eq!(reader.read_u32::<LittleEndian>().unwrap(), 8 + 4);
+    assert_eq!(reader.read_u32::<LittleEndian>().unwrap() as usize,
+               8 + 4);
     // tuple_ptr
-    assert_eq!(reader.read_u32::<LittleEndian>().unwrap(), PAGE_SIZE - 16);
+    assert_eq!(reader.read_u32::<LittleEndian>().unwrap() as usize,
+               PAGE_SIZE - 16);
     // tuple_data
     for byte in read_lock[(PAGE_SIZE - 16) as usize
             ..PAGE_SIZE as usize].iter() {
@@ -38,51 +41,51 @@ fn test_write_new_tuple() {
 
 #[test]
 fn test_get_tuple_range() {
-    let mut buffer: [u8; PAGE_SIZE as usize] = [0; PAGE_SIZE as usize];
+    let mut buffer: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
     // upper_ptr
-    LittleEndian::write_u32(&mut buffer[0..4], PAGE_SIZE - 16);
+    LittleEndian::write_u32(&mut buffer[0..4], (PAGE_SIZE - 16) as u32);
     // lower_ptr
-    LittleEndian::write_u32(&mut buffer[4..8], 12);
+    LittleEndian::write_u32(&mut buffer[4..8], 16);
     // tuple_ptr 1
-    LittleEndian::write_u32(&mut buffer[8..12], PAGE_SIZE - 8);
+    LittleEndian::write_u32(&mut buffer[8..12], (PAGE_SIZE - 8) as u32);
     // tuple_ptr 2
-    LittleEndian::write_u32(&mut buffer[12..16], PAGE_SIZE - 16);
+    LittleEndian::write_u32(&mut buffer[12..16], (PAGE_SIZE - 16) as u32);
 
     let buf_page = BufPage::load_from(
         &buffer, &BufKey::new(0, 0)).unwrap();
 
-    let invalid_tuple_ptr = TuplePtr::new(BufKey::new(0, 1), 8);
+    let invalid_tuple_ptr = TuplePtr::new(BufKey::new(0, 1), 4);
     assert!(buf_page.get_tuple_data_range(&invalid_tuple_ptr).is_err());
 
-    let tuple_ptr_1 = TuplePtr::new(BufKey::new(0, 0), 8);
+    let tuple_ptr_1 = TuplePtr::new(BufKey::new(0, 0), 0);
     let tuple_range_1 = buf_page.get_tuple_data_range(&tuple_ptr_1).unwrap();
-    assert_eq!(tuple_range_1.start as u32, PAGE_SIZE - 8);
-    assert_eq!(tuple_range_1.end as u32, PAGE_SIZE);
+    assert_eq!(tuple_range_1.start, PAGE_SIZE - 8);
+    assert_eq!(tuple_range_1.end, PAGE_SIZE);
 
-    let tuple_ptr_2 = TuplePtr::new(BufKey::new(0, 0), 12);
+    let tuple_ptr_2 = TuplePtr::new(BufKey::new(0, 0), 1);
     let tuple_range_2 = buf_page.get_tuple_data_range(&tuple_ptr_2).unwrap();
-    assert_eq!(tuple_range_2.start as u32, PAGE_SIZE - 16);
-    assert_eq!(tuple_range_2.end as u32, PAGE_SIZE - 8);
+    assert_eq!(tuple_range_2.start, PAGE_SIZE - 16);
+    assert_eq!(tuple_range_2.end, PAGE_SIZE - 8);
 }
 
 #[test]
 fn test_buf_page_iter() {
-    let mut buffer: [u8; PAGE_SIZE as usize] = [0; PAGE_SIZE as usize];
+    let mut buffer: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
     // upper_ptr
-    LittleEndian::write_u32(&mut buffer[0..4], PAGE_SIZE - 16);
+    LittleEndian::write_u32(&mut buffer[0..4], (PAGE_SIZE - 16) as u32);
     // lower_ptr
-    LittleEndian::write_u32(&mut buffer[4..8], 12);
+    LittleEndian::write_u32(&mut buffer[4..8], 16);
     // tuple_ptr 1
-    LittleEndian::write_u32(&mut buffer[8..12], PAGE_SIZE - 8);
+    LittleEndian::write_u32(&mut buffer[8..12], (PAGE_SIZE - 8) as u32);
     // tuple_ptr 2
-    LittleEndian::write_u32(&mut buffer[12..16], PAGE_SIZE - 16);
+    LittleEndian::write_u32(&mut buffer[12..16], (PAGE_SIZE - 16) as u32);
 
     let buf_page = BufPage::load_from(
         &buffer, &BufKey::new(0, 0)).unwrap();
 
     let mut iter = buf_page.iter();
     assert_eq!(iter.next().unwrap(),
-               ((PAGE_SIZE - 8) as usize..PAGE_SIZE as usize));
+               (PAGE_SIZE - 8..PAGE_SIZE));
     assert_eq!(iter.next().unwrap(),
-               ((PAGE_SIZE - 16) as usize..(PAGE_SIZE - 8) as usize));
+               (PAGE_SIZE - 16..PAGE_SIZE - 8));
 }
