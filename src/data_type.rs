b2bf281819f4ff158bd61ea6a0cf2ea58f41d945
Sieu Tsoi <sieut@umich.edu>
2019-01-30 23:21:41 -0500

    Add string_to_bytes in DataType

--- src/data_type.rs
+++ src/data_type.rs
@@ -8,4 +8,13 @@ impl DataType {
             &DataType::Char => Some(1),
         }
     }
+
+    pub fn string_to_bytes(&self, input: &str) -> Option<Vec<u8>> {
+        match self {
+            &DataType::Char => {
+                if input.len() == 1 { Some(input.as_bytes().to_vec()) }
+                else { None }
+            },
+        }
+    }
 }
