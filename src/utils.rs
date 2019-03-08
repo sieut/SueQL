d3efe3dfe7c182766c7793dea73db6835138d398
Sieu Tsoi <sieut@umich.edu>
2019-03-08 13:09:49 -0500

    Fix utils' create_new_file

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::Write;
-use storage::PAGE_SIZE;
 
 pub fn assert_data_len(data: &[u8], desired_len: usize) -> Result<(), std::io::Error> {
     if data.len() == desired_len {
@@ -13,7 +12,9 @@ pub fn assert_data_len(data: &[u8], desired_len: usize) -> Result<(), std::io::E
 }
 
 pub fn create_file(fname: &str) -> Result<(), std::io::Error> {
+    use storage::buf_page::BufPage;
+
     let mut file = File::create(fname)?;
-    file.write_all(&[0; PAGE_SIZE as usize])?;
+    file.write_all(&BufPage::default_buf())?;
     Ok(())
 }
