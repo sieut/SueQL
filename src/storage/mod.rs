2bccdc51f5e2306c532275e545e5b9683fc0d6dc
Sieu Tsoi <sieut@umich.edu>
2019-03-21 17:10:14 -0400

    Add Storable trait for fixed-sized items

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,6 +3,12 @@ pub const PAGE_SIZE: usize = 4096;
 pub mod buf_key;
 pub mod buf_mgr;
 pub mod buf_page;
+pub mod storable;
+
+pub use self::buf_key::BufKey;
+pub use self::buf_mgr::BufMgr;
+pub use self::buf_page::BufPage;
+pub use self::storable::Storable;
 
 #[cfg(test)]
 mod tests;
