2c94df40d73d556c5617b979bcd63142b558e78d
Sieu Tsoi <sieut@umich.edu>
2020-07-20 17:54:38 -0700

    Create hash index in create_table.rs

--- src/exec/create_table.rs
+++ src/exec/create_table.rs
@@ -1,8 +1,9 @@
 use data_type::DataType;
 use db_state::DbState;
-use error::Result;
+use error::{Error, Result};
 use exec::{DataStore, ExecNode};
-use nom_sql::CreateTableStatement;
+use index::IndexType;
+use nom_sql::{Column, CreateTableStatement, TableKey};
 use rel::Rel;
 use std::sync::Arc;
 use tuple::TupleDesc;
@@ -14,6 +15,22 @@ pub struct CreateTable {
 impl CreateTable {
     pub fn new(stmt: CreateTableStatement) -> CreateTable {
         CreateTable { stmt }
+    }
+
+    fn create_hash_index(
+        &self,
+        rel: &mut Rel,
+        cols: &Vec<Column>,
+        db_state: &mut DbState,
+    ) -> Result<()> {
+        let desc = rel.tuple_desc();
+        let cols = cols
+            .iter()
+            .map(|col| desc.attr_index(&col.name))
+            .collect::<Option<Vec<_>>>()
+            .ok_or(Error::Internal("Invalid column in key.".to_string()))?;
+        rel.new_index(cols, IndexType::Hash, db_state)?;
+        Ok(())
     }
 }
 
@@ -34,7 +51,19 @@ impl ExecNode for CreateTable {
             .map(|ref field| field.column.name.clone())
             .collect();
         let tuple_desc = TupleDesc::new(attr_types, attr_names);
-        Rel::new(self.stmt.table.name.clone(), tuple_desc, db_state)?;
+        let mut rel = Rel::new(self.stmt.table.name.clone(), tuple_desc, db_state)?;
+        if let Some(ref keys) = self.stmt.keys {
+            for key in keys.iter() {
+                match key {
+                    TableKey::PrimaryKey(cols) => {
+                        self.create_hash_index(&mut rel, cols, db_state)?
+                    }
+                    _ => {
+                        todo!("Only primary keys are supported.");
+                    }
+                }
+            }
+        }
         Ok(())
     }
 
